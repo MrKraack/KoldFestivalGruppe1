4c5bac3b24fc0f9705a125bdab78bda2d96ada58
MrKraack <MrKraack@gmail.com>
2022-12-06 18:23:18 +0100

    Begun working with api

--- src/views/ProgramOversigt.vue
+++ src/views/ProgramOversigt.vue
@@ -1,15 +1,91 @@
 <template>
-    <div>
+    <div class="home">
+      <h1>Program Oversigt</h1>
+      <div class="festivalEntry">
+        <div class="loading" v-if="loading">Loading...</div>
+        <div v-if="error" class="error">
+          {{ error }}
+        </div>
+        <!-- Container for friday -->
+        <!-- <div class="container">
+            <div v-for="entry in festivalEntry" :key="entry._id">
+                <div v-if="entry.location == 'Godset'">
+                    <h1>Hello {{entry.title}}</h1>
 
+                </div>
+                <div v-else>
+                    <h1>Not Godset {{entry.title}}</h1>
+
+                </div>
+                
+            </div>
+
+        </div> -->
+
+        <!-- Container for Saturday -->
+        <div class="container">
+          <div v-for="entry in festivalEntry" class="post-item" :key="entry._id">
+              <h2>{{ entry.title }}</h2>
+            <p>{{entry.location}}</p>
+            <hr />
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-
-<script>
-    export default {
-        
-    }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script>
+  import sanity from "../client";
+  
+  const query = `*[_type == "festivalentry"]{
+    _id,
+    title,
+    concertTime,
+    location
+  }[0...50]`;
+  
+  export default {
+    name: "ProgramOversigt",
+    data() {
+      return {
+        loading: true,
+        festivalEntry: [],
+      };
+    },
+    created() {
+      this.fetchData();
+    },
+    methods: {
+      fetchData() {
+        this.error = this.post = null;
+        this.loading = true;
+        sanity.fetch(query).then(
+          (festivalEntry) => {
+            this.loading = false;
+            this.festivalEntry = festivalEntry;
+            console.log(this.festivalEntry);
+          },
+          (error) => {
+            this.error = error;
+          }
+        );
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .home h1{
+      text-align: center;
+  
+  }
+  .container {
+    margin: 0 auto;
+    max-width: 42em;
+    width: 100%;
+  }
+  .post-item {
+    box-sizing: border-box;
+  }
+  </style>
+  
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -9,6 +9,7 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import programoversigtentry from './programOversigtEntry'
 import testing from './testing'
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -23,6 +24,7 @@ export default createSchema({
     post,
     author,
     category,
+    programoversigtentry,
     testing,
 
     // When added to this list, object types can be used as
