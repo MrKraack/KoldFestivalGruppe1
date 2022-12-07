8daa0c1509b7646316b6177c9c010ade4ad5b207
Dalena <79627422+dalenatrinh@users.noreply.github.com>
2022-12-07 08:57:24 +0100

    noget af info tilføjet

---
+++ studio/schemas/info.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'info',
+    title: 'Info og Faq',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'body',
+        title: 'Body',
+        type: 'blockContent',
+      },
+    ],
+  }
