67de6b5712dbfb1c471388d1b15e30d1ddeb08a8
caldecreativo <93321331+caldecreativo@users.noreply.github.com>
2022-12-07 09:37:54 +0100

    ticket component

---
+++ src/components/CompBilletType.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <section>
+            
+            <h1></h1>
+
+            <div>
+                <p></p> <p></p>
+                <p></p> <p></p>
+                <p></p> <p></p>
+            </div>
+
+            <button></button>
+        </section>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
--- src/views/BilletTyper.vue
+++ src/views/BilletTyper.vue
@@ -1,59 +1,17 @@
 <template>
     <div>
-        <section>
-        <img src="" alt="">
         
-        <div>
-        <h1></h1>
-        <h2></h2>
-        </div>
-
-        <p></p>
-
-        <section>
-
-            <h3></h3>
-
-            <div>
-                <p></p>
-                <p></p>
-            </div>
-
-            <div>
-                <p></p>
-                <p></p>
-            </div>
-
-            <div>
-                <p></p>
-                <p></p>
-            </div>
-
-            <button @click="buyTicket">Køb Billet <img src="" alt=""></button>
-
-        </section>
-    </section>
     </div>
 </template>
 
 <script>
+import CompBilletType from "../components/CompBilletType.vue";
     export default {
     name: 'BilletTyper',
-    data(){
-        return {
-            ticketDays: ["Fredag", "Lørdag", "Partout"],
-            ticketTypes: ["Ung/Studerende", "voksen"],
-            ticketAge: ["16-25 år eller gyldigt studie ID", "26+"],
-            ticketPrice: ["150 KR", "300 KR"]
-
-
-        }
-    },
-    methods: {
-        buyTicket(){
-
-        }
-    }      
+    components: {
+        CompBilletType
+    }
+    
     }
 </script>
 
