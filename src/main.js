0911ed7ba2c4f916dc4d85142bab8e2614194b15
Artur Pilarek <artur.pilarek@gmail.com>
2022-12-11 10:42:50 +0100

    Removed unused plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vueClickOutsideElement from 'vue-click-outside-element'
-import {Plugin} from "vue-responsive-video-background-player";
 
-createApp(App).use(router).use(vueClickOutsideElement).use(Plugin).mount('#app')
+createApp(App).use(router).use(vueClickOutsideElement).mount('#app')
