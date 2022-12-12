7af97e55a1bc252e02843757e7bbe1a3bb82bf9a
Anna-1902 <annaipsen19@gmail.com>
2022-12-12 20:12:41 +0100

    artist view with media query
    with carousel and grid view depending on screen size

--- src/components/ArtistCard.vue
+++ src/components/ArtistCard.vue
@@ -1,29 +1,88 @@
 <template>
-  <div>
-      <h3>{{ name }}</h3>
-      <p>{{ dateFormat(date) }}</p>
-      <p> {{ location }}</p>
+  <div class="artist-card"
+       :style="{backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)), url('+ image +')'}"
+  >
+    <section class="text-wrapper" :class="borderColor">
+        <h3>{{ name }}</h3>
+        <p>{{ day }} kl. {{ hour }}:{{ minute }}</p>
+      <section class="location">
+        <img src="../assets/location.png">
+        <p> {{ location }}</p>
+      </section>
+    </section>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
-
 export default {
   name: "artistCard",
   props: {
     name: String,
-    date: String,
+    day: String,
+    hour: String,
+    minute: String,
     location: String,
+    image: String ,
   },
-  methods: {
-    dateFormat(date) {
-      return moment(date).format('MM/DD/YYYY HH:mm')
+  computed: {
+    borderColor(){
+      return this.day === 'Fredag' ? 'greenBorder' : 'blueBorder'
     }
-  },
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.artist-card {
+  padding: 1rem;
+  width: fit-content;
+  text-transform: uppercase;
+  border-radius: 20px;
+  background-size: cover;
+  background-position: center;
+  transition: transform .3s ease-in-out;
+  h3 {
+    font-size: $text-large;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: $text-xs;
+    margin: 0;
+    color: white;
+  }
+  &:hover {
+    transform: scale(1.15);
+  }
+  .greenBorder{
+    border: $neon-green 1px solid;
+    h3 {
+      color: $neon-green;
+    }
+  }
+  .blueBorder{
+    border: $neon-turquoise 1px solid;
+    h3 {
+      color: $neon-turquoise;
+    }
+  }
+  .text-wrapper {
+    @include flex-column;
+    align-items: flex-start;
+    justify-content: end;
+    gap: .5rem;
+    height: 9rem;
+    width: 9rem;
+    border-radius: 20px;
+    padding: 0 1rem 1.5rem 1rem;
+    .location {
+      @include flex-row;
+      gap: .5rem;
+      align-items: baseline;
+      img {
+        height: .5rem;
+        width: auto;
+      }
+  }
+  }
+}
 </style>
--- src/components/ArtistContainer.vue
+++ src/components/ArtistContainer.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
-    <artist-card v-for="artist in artists"
-                 :key="artist._id"
-                 :name="artist.name"
-                 :date="artist.playTime"
-                 :location="artist.location"
-    ></artist-card>
+      <div class="days">
+        <section>
+          <div class="friday"/>
+          <p>Fredag</p>
+        </section>
+        <section>
+          <div class="saturday"/>
+          <p>Lørdag</p>
+        </section>
+      </div>
+      <div class="artist-container">
+        <artist-card v-for="artist in artists"
+                     :key="artist._id"
+                     :name="artist.name"
+                     :day="artist.playTime.day"
+                     :hour="artist.playTime.time.hour"
+                     :minute="artist.playTime.time.minute"
+                     :location="artist.location"
+                     :image="artist.imageUrl"
+                     @click="openModal(artist)"
+        />
+        <artist-modal v-show="showModal" v-if="!modalLoad" @close-modal="showModal = false"
+                      :artist-name="showArtistObject.name"
+                      :artist-img="showArtistObject.imageUrl"
+                      :artist-day="showArtistObject.playTime.day"
+                      :artist-hour="showArtistObject.playTime.time.hour"
+                      :artist-minute="showArtistObject.playTime.time.hour"
+                      :location="showArtistObject.location"
+                      :description="showArtistObject.description"
+                      :video="showArtistObject.video"
+        />
+        <p v-show="loading">Loading .... </p>
+      </div>
   </div>
 </template>
 
 <script>
 import sanity from "../client";
 import artistCard from "@/components/ArtistCard";
+import artistModal from "@/components/ArtistModal";
+import {toRaw} from "vue";
 
 const query = `*[_type == "artist"]{
   _id,
@@ -20,30 +49,36 @@ const query = `*[_type == "artist"]{
   playTime,
   location,
   description,
-  image
+  video,
+  'imageUrl': image.asset->url
 }`;
 
 export default {
   name: "artistContainer",
   components: {
-    artistCard
+    artistCard,
+    artistModal
   },
 
   data() {
     return {
       loading: true,
       artists: [],
+      showModal: false,
+      showArtistObject: '',
+      modalLoad: true,
     };
   },
   created() {
     this.fetchData();
   },
   methods: {
-    fetchData() {
+    async fetchData() {
       this.error = this.artists = null;
-      sanity.fetch(query).then(
+      await sanity.fetch(query).then(
           (artists) => {
             this.artists = artists;
+            this.loading = false
 
             console.log(artists)
           },
@@ -52,10 +87,47 @@ export default {
           }
       );
     },
+    openModal (artist) {
+        this.showArtistObject = toRaw(artist)
+      console.log(this.showArtistObject)
+        this.modalLoad = false
+        this.showModal = true
+    }
   },
 }
 </script>
 
 <style scoped lang="scss">
-
+.days {
+  @include flex-row;
+  justify-content: flex-start;
+  gap: 2rem;
+  section {
+    @include flex-row;
+    align-items: center;
+    gap: .5rem;
+    p {
+      text-transform: uppercase;
+      font-size: $text-xs;
+    }
+    .friday {
+      border: 1px solid $neon-green;
+      width: 1rem;
+      height: 0;
+    }
+    .saturday {
+      border: 1px solid $neon-turquoise;
+      width: 1rem;
+      height: 0;
+    }
+  }
+}
+.artist-container {
+  @include flex-row;
+  padding: 2rem;
+  gap: 2rem;
+  justify-content: center;
+  border: $neon-green solid 2px;
+  border-radius: 20px;
+}
 </style>
