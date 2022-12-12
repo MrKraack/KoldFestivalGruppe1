2d5dd1917e1d9680cbfe148a82779289991356a4
Anna-1902 <annaipsen19@gmail.com>
2022-12-12 20:13:21 +0100

    Section header
    Section header component depending on prop with color

---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <img :src="resolveSrc(this.color)" alt="hands" class="notMobile">
+    <img :src="resolveSrcMobile(this.color)" alt="hands" class="mobile">
+    <section class="heading">
+      <h3 :style="{textShadow: `0 0 5px #fff,
+                  0 0 10px #fff,
+                  0 0 15px ${resolveColor},
+                  0 0 20px ${resolveColor}`}"
+      >{{firstHeading}}</h3>
+      <h4 :style="{color: resolveColor}">{{secondHeading}}</h4>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sectionHeader",
+  props: {
+    color: String,
+    firstHeading: String,
+    secondHeading: String,
+  },
+  methods: {
+    resolveSrc(color) {
+      return require(`../assets/hands/${color}handsweb.png`)
+    },
+    resolveSrcMobile(color) {
+      return require(`../assets/hands/${color}hands.png`)
+    },
+  },
+  computed: {
+    resolveColor() {
+      switch (this.color) {
+        case 'green':
+          return '#00FF01';
+        case 'blue':
+          return '#06F2DB';
+        case 'orange':
+          return '#FF9201';
+        case 'pink':
+          return '#FE2390';
+        default:
+          return 'hotpink'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img {
+  width: 100vw;
+  height: auto;
+}
+.heading {
+  text-align: center;
+  font-size: $neon-undertitle-desktop;
+  text-transform: uppercase;
+  background: $bg-color;
+  h4 {
+   margin-top: -1.5rem;
+  }
+  h3 {
+    margin: 0;
+    color: $bg-color;
+    text-align: center;
+  }
+}
+
+.mobile {
+  display: none;
+}
+.notMobile {
+  display: block;
+}
+
+$screen-mobile: 390px;
+@media screen and (max-width: $screen-mobile) {
+  .mobile {
+    display: block;
+  }
+  .notMobile {
+    display: none;
+  }
+}
+</style>
